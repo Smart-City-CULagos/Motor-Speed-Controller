fad50ab69333c0c4f97a5c49f4708af9c8d3afac
KawaiiCyberKnight <55341734+KawaiiCyberKnight@users.noreply.github.com>
2019-09-28 01:02:28 -0500

    Create constants.h

---
+++ Serial/constants.h
@@ -0,0 +1,9 @@
+#define DEBUG true
+
+#define ENABLE 0 //Enable Pin of the H bridge
+#define INA 1 //Input A of the H bridge
+#define INB 2 //Input B of the H bridge
+
+#define Kp 1 //Proportional Constant
+#define Ki 1 //Integral Constant
+#define Kd 1 //Derivative Constant
